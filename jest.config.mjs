90151f0020329cb5b1a531f79d563b5e9cb15218
João Mangueira <jamjs7@gmail.com>
2024-07-04 11:39:10 -0300

    primeiro commit

---
+++ jest.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https:
+
+/** @type {import('jest').Config} */
+const config = {
+  learMocks: true,
+
+  overageProvider: "v8",
+}
+export default config;
---
+++ test/template.test.js
@@ -0,0 +1,9 @@
+import { it, expect } from '@jest/globals'
+
+function sum(a, b) {
+    return a + b
+}
+
+it('sum tow values', () => {
+    expect(sum(2,3)).toBe(5)
+})
