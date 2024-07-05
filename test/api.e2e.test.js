6bcc84dcaaf6976c9eb0d5b23443091d95c2ddcb
João Mangueira <jamjs7@gmail.com>
2024-07-05 12:41:07 -0300

    teste do index 100%

--- test/api.e2e.test.js
+++ test/api.e2e.test.js
@@ -1,15 +1,41 @@
-import { describe, it, expect, beforeAll, jest } from '@jest/globals'
-import { mapPerson } from '../src/person'
+import { describe, it, expect, beforeAll, afterAll, jest } from '@jest/globals'
 
 function waitForServerStatus(server) {
-    return new Promise((resolvee, reject) => {
+    return new Promise((resolve, reject) => {
         server.once('error', (err) => reject(err))
         server.once('listening', () => resolve())
     })
 }
 
 describe('E2E Teste Suite', () => {
-    describe('E2E Teest for Server', () => {
+    describe('E2E Test for Server in a not-test ENV', () => {
+        it('should start server with PORT 4000', async () => {
+            const PORT = 4000
+            process.env.NODE_ENV = 'production'
+            process.env.PORT = PORT
+
+            jest.spyOn(
+                console,
+                console.log.name
+            )
+            
+            const { default: server } = await import('../src/index.js')
+              
+            await waitForServerStatus(server)
+            const serverInfo = server.address()
+
+            //console.log(`server is runnig at ${serverInfo.address}:${serverInfo.port}`)
+
+            expect(serverInfo.port).toBe(4000)
+            expect(console.log).toHaveBeenCalledWith(
+                `server is runnig at ${serverInfo.address}:${serverInfo.port}`
+            )
+
+            return new Promise(resolve => server.close(resolve))
+        })
+
+    })
+    describe('E2E Test for Server', () => {
         let _testServer
         let _testServerAddress
 
@@ -21,29 +47,36 @@ describe('E2E Teste Suite', () => {
             await waitForServerStatus(_testServer)
             const serverInfo = _testServer.address()
             _testServerAddress = `http://localhost:${serverInfo.port}`
-
         })
+
+        afterAll(() => _testServer.close())
 
         it('should return 404 for unsupported routes', async () => {
-            const personStr = '{"name": "joao", "age":37}'
-            const personObj = mapPerson(personStr)
-    
-            expect(personObj).toEqual({
-                name: 'joao',
-                age: 37,
-                createdAt: expect.any(Date)
+            const response = await fetch(`${_testServerAddress}/unsupported`, {
+                method: 'POST'
             })
+
+            expect(response.status).toBe(404)
         })
 
-        it.todo('should return 400 and missing file message when body is invalid', async () => {
-            const personStr = '{"name": "joao", "age":37}'
-            const personObj = mapPerson(personStr)
-    
-            expect(personObj).toEqual({
-                name: 'joao',
-                age: 37,
-                createdAt: expect.any(Date)
+        it('should return 404 for unsupported routes type', async () => {
+            const response = await fetch(`${_testServerAddress}/persons`, {
+                method: 'GET'
             })
+
+            expect(response.status).toBe(404)
+        })
+
+        it('should return 400 and missing file message when body is invalid', async () => {
+            const invalidPerson = { name: 'Fulano da Silva'}
+            const response = await fetch(`${_testServerAddress}/persons`, {
+                method: 'POST',
+                body: JSON.stringify(invalidPerson)
+            })
+
+            expect(response.status).toBe(400)
+            const data = await response.json()
+            expect(data.validationError).toEqual('cpf is required')
         })
     })    
 })
